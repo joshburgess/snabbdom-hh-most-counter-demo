edf8a0b9e7f7a1c8d459dd8ec018b727f413f4b3
joshburgess <joshburgess.webdev@gmail.com>
2016-09-13 23:13:03 -0400

    more cleanup

--- src/custom-helpers/counter.js
+++ src/custom-helpers/counter.js
@@ -1,20 +1,17 @@
 import h from 'snabbdom/h'
 import hh from 'hyperscript-helpers'
-const { h1, h2, div, br } = hh(h) // eslint-disable-line fp/no-unused-expression
+const { h1, h2, div, br } = hh(h)
 import { btn } from './'
 
-const counter = ({ title, subtitle, decrement, increment }, state) => {
-  return div('.counter', [
+const counter = ({ title, subtitle, decrement, increment }, state) =>
+  div('.counter', [
     h1(title),
     h2(subtitle),
     br(),
     br(),
-    // div('.counter-btn', { on: { click: decrement } }, '-'),
     btn('-', decrement),
     h1(state),
-    // div('.counter-btn', { on: { click: increment } }, '+'),
     btn('+', increment),
   ])
-}
 
 export default counter
